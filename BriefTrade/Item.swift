c1baf00ad12f637713bad1bff478f7dc4302ca77
YvonneJin1826 <161699259+YvonneJin1826@users.noreply.github.com>
2026-02-25 11:23:08 +0800

    Initial Commit

---
+++ BriefTrade/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BriefTrade
+//
+//  Created by JinZhaoXi on 2026/2/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BriefTradeTests/BriefTradeTests.swift
@@ -0,0 +1,16 @@
+//
+//  BriefTradeTests.swift
+//  BriefTradeTests
+//
+//  Created by JinZhaoXi on 2026/2/25.
+//
+
+import Testing
+
+struct BriefTradeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
